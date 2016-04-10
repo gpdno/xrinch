6bb196b8697c853181cf362f88a51ef99b343811
Gregory DeNinno <gpdno@mac.com>
2016-04-10 16:51:57 +0000

    added detail controller and pages

---
+++ app/controllers/details_controller.rb
@@ -0,0 +1,6 @@
+class DetailsController < ApplicationController
+    
+    def index
+    
+    end    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   root to: 'home#index'
   
-  resources :weather_grid
+  resources :details
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
