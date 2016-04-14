abae6e358c8b1b1a9e1ea98b3bc2e54398858e52
Gregory DeNinno <gpdno@mac.com>
2016-04-14 02:56:11 +0000

    removed unused method in Portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,13 +4,4 @@ class Portfolio < ActiveRecord::Base
     validates :name, presence: true, length: {minimum: 3, maximum: 150}
     validates :description, presence: true, length: {minimum: 10, maximum: 7500}
     
-# This method associates the attribute ":avatar" with a file attachment
-  has_attached_file :avatar, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
-
-  # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
