986c8fa0b2ff23e75917c7a6e8283a861c8c7b10
Gregory DeNinno <gpdno@mac.com>
2016-04-14 02:15:17 +0000

    updated config files for picture uploader

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,13 +6,13 @@ if Rails.env.production?
     
         :provider => 'AWS',
         
-        :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+        :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
+        :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     
     }
     
-    config.fog_directory = ENV['S3_BUCKET']
+    config.fog_directory = ENV['S3_BUCKET_NAME']
     
     end
 
